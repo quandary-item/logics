63325d3c70d47226c0785cd2cf391d331cabcfd4
C A <consolation.archaism@protonmail.com>
2019-08-18 13:21:58 +0100

    allow a rule to be applied to part of a formula

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,18 @@ data Logic t = Val t | Bin BinOp (Logic t) (Logic t) deriving (Eq, Show)
 
 data CompoundRule = DoLeft CompoundRule | DoRight CompoundRule | Do Rule deriving Show
 
+applyCompoundRule :: (Eq t) => CompoundRule -> Logic t -> Either () (Logic t)
+applyCompoundRule (DoLeft rule) (Bin binOp l r) = do
+  l' <- applyCompoundRule rule l
+  pure $ Bin binOp l' r
+
+applyCompoundRule (DoRight rule) (Bin binOp l r) = do
+  r' <- applyCompoundRule rule r
+  pure $ Bin binOp l r'
+
+applyCompoundRule (Do rule) logic = applyRule rule logic
+
+
 data Rule = Commutation BinOp
           | Association1 BinOp
           | Association2 BinOp          
