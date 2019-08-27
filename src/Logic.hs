62a1b0c20d7321a48d9a586ccf726a3e2a68ff8c
C A <consolation.archaism@protonmail.com>
2019-08-27 14:47:07 +0100

    add wip for unification on logical exps

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,6 +1,23 @@
 module Logic where
 
+import qualified Data.Map as Map
+
 data BinOp = Conj | Disj | XOr | Impl deriving (Eq, Show)
 data UnOp = Neg deriving (Eq, Show)
 
 data Logic t = Val t | Un UnOp (Logic t) | Bin BinOp (Logic t) (Logic t) deriving (Eq, Show)
+
+
+replace :: (Ord t) => Logic t -> Map.Map t (Logic t) -> Logic t
+replace (Val a) assignment = case (Map.lookup a assignment) of
+  Just p  -> p
+  Nothing -> Val a
+
+replace (Un unOp l) assignment = Un unOp l'
+  where
+    l' = replace l assignment
+
+replace (Bin unOp l r) assignment = Bin unOp l' r'
+  where
+    l' = replace l assignment
+    r' = replace r assignment
---
+++ src/Unification.hs
@@ -0,0 +1,35 @@
+module Unification where
+
+import Logic
+
+-- given the antecendent or consequent of a nat deduction rule,
+-- try to match it with a given formula and return what values must be mapped to
+-- make them match
+
+-- e.g.
+-- A, P, [] -> [A -> P]
+-- A, P, [A -> Q] -> Nothing
+
+unify :: (Ord t, Eq t') => Logic t -> Logic t' -> Map.Map t (Logic t') -> Maybe (Map.Map t (Logic t'))
+unify (Val a) p assignment = case lookupRes of
+  Just v -> if v == p then (Just $ Map.insert a p assignment) else Nothing
+  _      -> Just $ Map.insert a p assignment
+  where
+    lookupRes = Map.lookup a assignment
+
+unify (Un unOp a) (Un unOp' b) assignment
+  | unOp == unOp' = unify a b assignment
+  | otherwise     = Nothing
+
+unify (Un _ _) _ _ = Nothing
+
+-- TODO: redo using do-notation
+unify (Bin binOp l r) (Bin binOp' l' r') assignment
+  | binOp == binOp' = case (unify l l' assignment) of
+      Just assignment' -> case (unify r r' assignment') of
+        Just assignment'' -> Just assignment''
+        _ -> Nothing
+      _ -> Nothing
+  | otherwise = Nothing
+
+unify (Bin _ _ _) _ _ = Nothing
