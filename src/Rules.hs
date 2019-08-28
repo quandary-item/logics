e0c7a5bbb8ac9341bb5e71d891120bdacbc37b93
C A <consolation.archaism@protonmail.com>
2019-08-28 11:32:07 +0100

    add more rules

--- src/Rules.hs
+++ src/Rules.hs
@@ -5,22 +5,44 @@ import Unification
 
 data Syms = AA | BB | CC | DD deriving (Eq, Ord, Show)
 
+vA = Val AA
+vB = Val BB
+vC = Val CC
+vD = Val DD
+
 type Rule' = (Logic Syms, Logic Syms)
 
 elimXOr :: Rule'
-elimXOr = ( Bin XOr (Val AA) (Val BB)
-          , Bin Conj (Bin Disj (Val AA) (Val AA)) (Un Neg $ Bin Conj (Val AA) (Val BB))
+elimXOr = ( Bin XOr vA vB
+          , Bin Conj (Bin Disj vA vB) (Un Neg $ Bin Conj vA vB)
           )
 
 commutes :: BinOp -> Rule'
-commutes binOp = ( Bin binOp (Val AA) (Val BB)
-                 , Bin binOp (Val BB) (Val AA)
+commutes binOp = ( Bin binOp vA vB
+                 , Bin binOp vB vA
                  )
 
 associates :: BinOp -> Rule'
-associates binOp = ( Bin binOp (Val AA) (Bin binOp (Val BB) (Val CC))
-                   , Bin binOp (Bin binOp (Val AA) (Val BB)) (Val CC)
+associates binOp = ( Bin binOp vA (Bin binOp vB vC)
+                   , Bin binOp (Bin binOp vA vB) vC
                    )
+
+distributes :: BinOp -> BinOp -> Rule'
+distributes binOp1 binOp2 = ( Bin binOp1 vA (Bin binOp2 vB vC)
+                            , Bin binOp2 (Bin binOp1 vA vB) (Bin binOp1 vA vC)
+                            )
+
+allRules = [ elimXOr
+           , commutes Conj
+           , commutes Disj
+           , commutes XOr
+           , associates Conj
+           , associates Disj
+           , associates XOr
+           , distributes Conj Conj
+           , distributes Conj Disj
+           , distributes Conj XOr
+           ]
 
 applyRuleForward :: (Eq t) => Rule' -> Logic t -> Maybe (Logic t)
 applyRuleForward (l, r) logic = case (unify' l logic) of
