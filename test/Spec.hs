3ab3a20bed3ff079b32dbd8e0d035cf69f7a0613
C A <consolation.archaism@protonmail.com>
2019-08-28 11:13:33 +0100

    write down some test ideas

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,4 +15,15 @@ instance (Arbitrary t) => Arbitrary (Logic t) where
   arbitrary = genericArbitraryU
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  pure
+
+-- Test ideas:
+-- Create a random rule and a random expression
+-- If the rule can be applied to the expression, then ????
+
+-- replace passes if all of the variables in the exp are provided in the mapping
+-- maybe need an aux method to extract the set of variables in an exp?
+-- this aux method should be tested too by itself. idk how
+
+-- if a rule can be evaluated forwards, it can be evaluated backwards with the result. i.e. it must be able to be evaluated symmetrically
