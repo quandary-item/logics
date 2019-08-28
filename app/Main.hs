72676a9a0d19434cc5a77852a1025c220f5a651a
C A <consolation.archaism@protonmail.com>
2019-08-28 11:33:32 +0100

    remove old rule implementation

--- app/Main.hs
+++ app/Main.hs
@@ -25,48 +25,6 @@ data Rule = Commutation BinOp
           | Intro BinOp
           | Elim BinOp deriving Show
 
-commutes :: BinOp -> Bool
-commutes binOp = binOp `elem` [Conj, Disj, XOr]
-
-associates :: BinOp -> Bool
-associates binOp = binOp `elem` [Conj, Disj, XOr]
-
-distributes :: BinOp -> BinOp -> Bool
-distributes Conj Conj = True
-distributes Conj Disj = True
-distributes Conj XOr = True
-distributes _ _ = False
-
-applyRule :: (Eq t) => Rule -> Logic t -> Either () (Logic t)
-applyRule (Commutation binOp) (Bin binOp' l r)
-  | binOp == binOp' && commutes binOp = Right $ Bin binOp r l
-  | otherwise                         = Left ()
-
-applyRule (Association1 binOp) (Bin binOp' (Bin binOp'' a b) c)
-  | binOp == binOp' && binOp' == binOp'' && associates binOp = Right $ Bin binOp a (Bin binOp b c)
-  | otherwise       = Left ()
-
-applyRule (Association2 binOp) (Bin binOp' a (Bin binOp'' b c))
-  | binOp == binOp' && binOp' == binOp'' && associates binOp = Right $ Bin binOp (Bin binOp a b) c
-  | otherwise       = Left ()
-
-applyRule (Distribution1 binOp1 binOp2) (Bin binOp' a (Bin binOp'' b c))
-  | binOp1 == binOp' && binOp2 == binOp'' && distributes binOp1 binOp2 = Right $ Bin binOp'' (Bin binOp' a b) (Bin binOp' a c)
-  | otherwise = Left ()
-
-applyRule (Distribution2 binOp1 binOp2) (Bin binOp'' (Bin binOp' a b) (Bin binOp''' a' c))
-  | a == a' && binOp1 == binOp' && binOp2 == binOp'' && binOp' == binOp''' && distributes binOp1 binOp2 = Right $ (Bin binOp' a (Bin binOp'' b c))
-  | otherwise = Left ()
-
-applyRule (Elim XOr) (Bin binOp l r)
-  | binOp == XOr = Right $ Bin Conj (Bin Disj l r) (Un Neg $ Bin Conj l r)
-  | otherwise    = Left ()
-
-applyRule (Intro XOr) (Bin Conj (Bin Disj l r) (Un Neg (Bin Conj l' r')))
-  | l == l' && r == r' = Right $ Bin XOr l r
-  | otherwise    = Left ()
-
-
 applyRule _ _ = Left ()
 
 doRules :: (Eq t, Show t) => (Logic t) -> [CompoundRule] -> IO ()
